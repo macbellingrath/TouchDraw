e0fcb146b5cdbedc558ac121b2fc50caf4d7eba5
macbellingrath <macbellingrath@gmail.com>
2015-10-01 15:55:42 -0400

    Class Work on Drawing App

--- DrawSomething/ToggleButton.swift
+++ DrawSomething/ToggleButton.swift
@@ -8,56 +8,36 @@
 
 import UIKit
 
-@IBDesignable
-
-class ToggleButton: UIButton {
+@IBDesignable class ToggleButton: UIButton {
     
     @IBInspectable var strokeWidth: CGFloat = 1
+    @IBInspectable var circleInset: CGFloat = 10
     
-    @IBInspectable var circleInset: CGFloat = 0
-    
-//    @IBInspectable var indicator: DrawView
-    
-
-        override func drawRect(rect: CGRect) {
-            
-            let midX = CGRectGetMidX(rect)
-            let midY = CGRectGetMidY(rect)
-            
-      
-            
-            let context = UIGraphicsGetCurrentContext()
-            
-            let insetRect = CGRectInset(rect, circleInset, circleInset)
-            
-            tintColor.set()
-            
-            CGContextSetLineWidth(context, strokeWidth)
-            
-            CGContextStrokeEllipseInRect(context, insetRect)
-            
-            
-            
-            CGContextMoveToPoint(context, circleInset + 10, midY - 2)
-         
-            CGContextAddLineToPoint(context, midX, midY + 8)
-           
-            CGContextAddLineToPoint(context, rect.width - circleInset - 10, midY - 3 )
-           
-            
-            CGContextStrokePath(context)
-            
-            
-            
-            
-            
-            
-            
-           
-            
+    override func drawRect(rect: CGRect) {
         
-            
+        let context = UIGraphicsGetCurrentContext()
+        
+        let insetRect = CGRectInset(rect, circleInset, circleInset)
+        
+        tintColor.set()
+        
+        CGContextSetLineWidth(context, strokeWidth)
+        
+        CGContextStrokeEllipseInRect(context, insetRect)
+        
+        let midX = CGRectGetMidX(rect)
+        let midY = CGRectGetMidY(rect)
+        
+        CGContextMoveToPoint(context, circleInset + 10, midY - 3)
+        CGContextAddLineToPoint(context, midX, midY + 6)
+        CGContextAddLineToPoint(context, rect.width - circleInset - 10, midY - 3)
+        
+        CGContextStrokePath(context)
+        
     }
-
-
+    
 }
+
+
+
+
--- DrawSomething/ViewController.swift
+++ DrawSomething/ViewController.swift
@@ -2,159 +2,171 @@
 //  ViewController.swift
 //  DrawSomething
 //
-//  Created by Mac Bellingrath on 9/29/15.
+//  Created by Mac Bellingrath on 10/1/15.
 //  Copyright © 2015 Mac Bellingrath. All rights reserved.
 //
 
 import UIKit
 
+class ViewController: UIViewController {
 
-//// Normal
-//
-//- Create a New App "DrawSomething"
-//    - Create a New File "DrawView" based on "UIView"
-//        - Using "CGContextRef"
-//            - draw a blue circle on the view
-//                - draw a square with a red outline that is 10 pt thick
-//
-//// Hard
-//
-//- Add "BeginTouches" Function
-//- Print "CGPoint" from location of touch
-//- Using "CGContextRef"
-//- draw a green triangle
-//
-//// Nightmare
-//
-//- Using "CGContextRef"
-//- draw a scribble using arcs
-//
-//jo [4:45 PM]
-
-
-class ViewController: UIViewController, UITextFieldDelegate {
-
-  
-
-    @IBOutlet weak var controlPanelTop: NSLayoutConstraint!
-   
-    @IBOutlet weak var controlPanelView: UIView!
-
-   
-    @IBAction func expandMenuPressed(sender: ToggleButton) {
-       
-        controlPanelTop.constant = controlPanelView.frame.origin.y == 0 ? -200 : 0
-        
-        print(controlPanelTop.constant)
-        
-     UIView.animateWithDuration(2.0) { () -> Void in
     
-        self.view.layoutIfNeeded()
-
+    @IBOutlet weak var controlPanelTop: NSLayoutConstraint!
+    
+    @IBOutlet weak var controlPanelView: UIView!
+    
+    @IBAction func toggleControlPanel(sender: AnyObject) {
         
-      
+        self.controlPanelTop.constant = self.controlPanelView.frame.origin.y == 0 ? -200 : 0
+        view.setNeedsUpdateConstraints()
         
-        
+        UIView.animateWithDuration(0.4) { () -> Void in
+            
+            self.view.layoutIfNeeded()
         }
         
-        
     }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    view.frame.origin.y = -200
-        
-        UIView.animateWithDuration(2.4) { () -> Void in
-            self.view.frame.origin.y = 0
-        }
-
-        
-        
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        UIApplication.sharedApplication().statusBarStyle = UIStatusBarStyle.LightContent
     }
     
-
-     
+    
+    
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         
-        
-        
-            if let touch = touches.first {
-               
-                //Line
-//                let newLine = Line()
-//                
-//                newLine.start = touch.locationInView(view)
-//                newLine.strokeColor = UIColor.blackColor()
-//                newLine.strokeWidth = 10
-//                
-//                (view as? DrawView)?.lines.append(newLine)
+        if let touch = touches.first {
+            
+            switch chosenTool {
+            case 1:
                 
-                //Scribble
+                let newScribble = Scribble()
                 
-//                let newScribble = Scribble()
-//                
-//                newScribble.points.append(touch.locationInView(view))
-//                
-//                newScribble.strokeWidth = 10
-//                newScribble.strokeColor = UIColor.blackColor()
-//                
-//                (view as? DrawView)?.lines.append(newScribble)
+                newScribble.points.append(touch.locationInView(view))
                 
-                ///// Shape
+                newScribble.strokeWidth = 10
+                newScribble.strokeColor = UIColor.blackColor()
                 
-                
-                if let currentShape = (view as? DrawView)?.lines.last {
-                    currentShape.end = touch.locationInView(view)
-                }
+                (view as? DrawView)?.lines.append(newScribble)
+            case 2:
+                ///// SHAPE
                 
                 let shape = Shape(type: .Circle)
                 
-                
                 shape.start = touch.locationInView(view)
-                
-                
-          
-                
                 shape.fillColor = UIColor.blackColor()
+                
                 (view as? DrawView)?.lines.append(shape)
-                
-          
-                
-                
                 
                 view.setNeedsDisplay()
                 
+
+            case 3: drawShape(withShape: .Rectangle, andTouch: touch)
+            case 4: drawShape(withShape: .Triangle, andTouch: touch)
+                
+            case 5:
+                drawShape(withShape: .Diamond, andTouch: touch)
+            default:
+                /// LINE
+                
+                            let newLine = Line()
+                
+                            newLine.start = touch.locationInView(view)
+                            newLine.strokeColor = UIColor.blackColor()
+                            newLine.strokeWidth = 10
+                
+                            (view as? DrawView)?.lines.append(newLine)
+                
+                
+            }
+
             
-        }
+                   }
+        
     }
     
     
+    
+    var chosenTool = 0
+    
+    
+    @IBAction func chooseTool(sender: UIButton) {
+        
+        chosenTool = sender.tag
+        
+        
+        
+    }
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
-      
+        
         if let touch = touches.first {
-            
-            let currentline = (view as? DrawView)?.lines.last
-
-            currentline?.end = touch.locationInView(view)
-            view.setNeedsDisplay()
             
             
             if let currentScribble = (view as? DrawView)?.lines.last as? Scribble {
                 currentScribble.points.append(touch.locationInView(view))
                 view.setNeedsDisplay()
                 
+            } else if let currentLine = (view as? DrawView)?.lines.last {
+                currentLine.end = touch.locationInView(view)
+                view.setNeedsDisplay()
             }
             
+            ///// LINE
+            //
+            //            if let currentLine = (view as? DrawView)?.lines.last {
+            //
+            //                currentLine.end = touch.locationInView(view)
+            //
+            //                view.setNeedsDisplay()
+            //
+            //            }
+            
+            ///// SCRIBBLE
+            
+            //            if let currentScribble = (view as? DrawView)?.lines.last as? Scribble {
+            //
+            //                currentScribble.points.append(touch.locationInView(view))
+            //
+            //                view.setNeedsDisplay()
+            //
+            //            }
+            
+            ///// SHAPE
+            
+            
+          
+            
+            
         }
+        
     }
     
-
-
-
+    func drawShape(withShape shape: ShapeType, andTouch touch: UITouch) {
+       
+        if let currentShape = (view as? DrawView)?.lines.last {
+            currentShape.end = touch.locationInView(view)
+        }
+        
+        let shape = Shape(type: shape)
+        
+        
+        shape.start = touch.locationInView(view)
+        
+        
+        
+        
+        shape.fillColor = UIColor.blackColor()
+        (view as? DrawView)?.lines.append(shape)
+        
+        
+        
+        
+        
+        view.setNeedsDisplay()
+    
+    
+}
 
 }
